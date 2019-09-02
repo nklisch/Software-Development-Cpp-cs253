edf48b916a1f3f61b07eb31409fa7a3fac434ba4
Nathan Klisch <nklisch@localhost.localdomain>
2019-09-01 22:34:03 -0600

    hw1

---
+++ cs370/HW1/Executor.h
@@ -0,0 +1,7 @@
+
+
+int get_iteration_count(int rand);
+int get_arr_size(int rand);
+char get_arr_val(int rand);
+float char_ratio(char *arr, int size, int *maxCountPointer);
+float get_running_ratio (int seed);
--- cs370/HW1/Invoker.c
+++ cs370/HW1/Invoker.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "Executor.h"
 
-int main()
+int main(int argc, const char* argv[])
 {
-	printf("Hello World");
+	int seed = atoi(argv[1]);
+	printf("Invoker - With Seed: %d\n", seed);
+	float ratio = get_running_ratio(seed);
+	printf("Invoker - Running Ratio: %f\n", ratio);
 	return 0;
 
 }
