ecb31dd5dcc114b859a34d6e70581eb19bda3d65
Nathan Klisch <nklisch@localhost.localdomain>
2019-09-05 19:53:27 -0600

    updates

--- cs253/HW1/hw1.cc
+++ cs253/HW1/hw1.cc
@@ -1,185 +1,236 @@
+//Nathan Klisch
+//cs253, Fall2019
+//HW1
 #include <iostream>
+#include <cctype>
 #include <string>
 #include <vector>
 #include <iomanip>
 
 using namespace std;
-string prgName; 
+string prgName;
 
-void printEnemy(const vector<string>&,const vector<string>&, int);
-bool isKeyunique(vector<string>&, string);
-bool isAlphabetic(string);
-bool getKey(string&, string&);
-bool getValue(string&,string&);
+void printEnemy(const vector<string> &, const vector<string> &, int);
+bool isKeyunique(const vector<string> &, const string&);
+bool isAlphabetic(const string &);
+bool getKey(string &, string &);
+bool getValue(string &, string &);
 //Finds index of first not blank char, or -1 if not found
-int findNotBlankChar(string&);
+int findNotBlankChar(const string &);
 //Finds index of first not blank char from the end of string, or -1 if not found
-int findNotBlankCharReverse(string&);
-bool isBlankLine(string&);
-bool readEnemy(vector<string>&, vector<string>&, string&);
-int getMaxCharLength(vector<string>&);
-//Returns true if cin see's a EOF character next
-bool isEoI();
-//Removes blank lines and sets line to the next non-blank line
-void removeBlankLines(string&);
+int findNotBlankCharReverse(const string &);
+//Finds index of first blank char from start of string of -1 if not found
+int findFirstBlankChar(const string &);
+bool isBlankLine(const string &);
+bool readEnemy(vector<string> &, vector<string> &, string &);
+//Gets the length of the longest key
+int getMaxCharLength(const vector<string> &);
 
-int main(int argc, char *argv[]){
-	prgName = argv[0];
+//Sticking to structs for ease until later
+struct Enemy{
 	vector<string> keys;
 	vector<string> values;
-	int maxKeyCharLength = argc;	
+	int maxKeyCharLength;
+};
+
+
+int main(int argc, char *argv[])
+{
+	prgName = argv[0];
+	if(argc > 2)
+		return 1;
+		
+	vector<Enemy> enemyList;
 	string line;
-        removeBlankLines(line);
-
-	while(!isEoI()) {
-		if(!readEnemy(keys,values,line))
-			return 1;
-		maxKeyCharLength = getMaxCharLength(keys);
-		printEnemy(keys,values,maxKeyCharLength);
+	while (getline(cin, line))
+	{
+		vector<string> keys;
+		vector<string> values;	
+		if (!isBlankLine(line))
+		{
+			if (!readEnemy(keys, values, line))
+				return 1;
+		}
 		
-		removeBlankLines(line);
-		if(isEoI())
-			return 0;
-		else 
-			cout << "\n";
+		if (!keys.empty())
+		{
+			Enemy newEnemy;
+			newEnemy.maxKeyCharLength = getMaxCharLength(keys);
+			newEnemy.keys = keys;
+			newEnemy.values = values;
+			enemyList.push_back(newEnemy);
+		}
 		
-		keys.clear();
-		values.clear();	
 	}
+	if(enemyList.empty())
+		return 0;
+	for( size_t i = 0; i < enemyList.size() - 1; i++) 
+	{
+		printEnemy(enemyList[i].keys, enemyList[i].values, enemyList[i].maxKeyCharLength);
+		cout << '\n';
+	}
+	printEnemy(enemyList.back().keys, enemyList.back().values, enemyList.back().maxKeyCharLength);
 	return 0;
-
-}
-bool isEoI(){
-	return cin.peek() == EOF;
 }
 
-void removeBlankLines(string &line){
-	while(cin.peek() != EOF && isBlankLine(line)){
-		getline(cin, line);
-	}
-}
-void printEnemy(const vector<string> &keys,const vector<string> &values, int maxKeyCharLength){
-	auto i = keys.size();
-	for(i = 0; i < keys.size(); i++){
-	cout << setw(maxKeyCharLength+1) << left;
-	cout << keys[i] << values[i] << "\n";	
+void printEnemy(const vector<string> &keys, const vector<string> &values, int maxKeyCharLength)
+{
+	for (size_t i = 0; i < keys.size(); i++)
+	{
+		cout << setw(maxKeyCharLength + 1) << left;
+		cout << keys[i] << values[i] << "\n";
 	}
 }
 
-bool isKeyUnique(vector<string>  &keys,const string key) {
-	for(string &k : keys){
-	if(key.compare(k) == 0)
-		return false;
+bool isKeyUnique(const vector<string> &keys, const string &key)
+{
+	for (const string &k : keys)
+	{
+		if (key == k)
+			return false;
 	}
 	return true;
 }
 
-bool isAlphabetic(string key){
-	for(auto c : key){
-		if(c < 'A' || c > 'Z'){
-			if(c < 'a' || c > 'z')
-				return false;
-			}
+bool isAlphabetic(const string &key)
+{
+	for (const auto c : key)
+	{
+		if (!isalpha(c))
+		{
+			return false;
 		}
+	}
 	return true;
-
 }
 
-bool getKey(string &key, string &line){
-	if(line[0] == ' ' || line[0] == '\t')
+bool getKey(string &key, string &line)
+{
+	if (isspace(line[0]))
 		return false;
-	
-	auto index = line.find(' ');
-	key = line.substr(0, index);
-	line = line.substr(index, line.length() - 1);
+
+	int index = findFirstBlankChar(line);
+	if (index > 0)
+	{
+		key = line.substr(0, index);
+		line = line.substr(index, line.length() - 1);
+	}
+	else
+	{
+		key = line;
+		line.clear();
+	}
+
 	return true;
 }
 
-bool getValue(string &value, string &line){
+bool getValue(string &value, string &line)
+{
 	int index = findNotBlankChar(line);
-	
-	if(index < 0)
+
+	if (index < 0)
 		return false;
+
 	int length = findNotBlankCharReverse(line) - index + 1;
-	
-	value = line.substr(index,length);
-	return true;	
+	value = line.substr(index, length);
+	return true;
 }
-int findNotBlankChar(string &line){
-	int i = 0;	
-	int index = -1;
-	for(auto c : line){
-		if(c != ' ' && c != '\t'){
-		index = i;
-		break;
+
+int findFirstBlankChar(const string &line)
+{
+
+	for (size_t i = 0; i < line.size(); i++)
+	{
+		if (isblank(line[i]))
+			return static_cast<int>(i);
+	}
+	return -1;
+}
+
+int findNotBlankChar(const string &line)
+{	
+	for (size_t i = 0; i < line.length(); i++)
+	{
+		if (!isspace(line[i]))
+		{
+			return static_cast<int>(i);
 		}
-	i++;
+	}
+	return -1;
+}
+
+int findNotBlankCharReverse(const string &line)
+{
+	int index = -1;
+	if (line.empty())
+		return -1;
+
+	for (auto i = line.length() - 1; i == 0; i--)
+	{
+		if (!isspace(line[i]))
+		{
+			index = static_cast<int>(i);
+			break;
+		}
 	}
 	return index;
 }
 
-int findNotBlankCharReverse(string &line){
-	int index = -1;
-	for(int i = line.length() - 1; i >= 0; i--){
-		if(line[i] != ' ' && line[i] != '\t'){
-		index = i;
-		break;
-		}
-	}
-	return index;
-}
-
-bool isBlankLine(string &line) {
-	if(findNotBlankChar(line) >= 0)
+bool isBlankLine(const string &line)
+{
+	if (findNotBlankChar(line) >= 0)
 		return false;
 
 	return true;
 }
 
-//Returns true if enemy is read succesfully, false if it fails for any reason
-bool readEnemy(vector<string> &keys,  vector<string> &
-values, string &line){
-	string key;
-	string value;
-	do{
-			
-		if(!getKey(key, line)) {
-		cerr << prgName << "- Key is missing!\n";
-		return false;
-		}
-		if(!isAlphabetic(key)) {
-		cerr << prgName << "- Key:" << key << " not alphabetic!\n";
-		return false;	
-		}
-		if(!getValue(value, line)) {
-		cerr << prgName << "- Value is missing!\n";
-		return false;
-		}
-		if(!isKeyUnique(keys, key)){
-		cerr << prgName << "- Key: " << key << " is not unique!\n";
-		return false;
-		}
-		
-		keys.push_back(key);
-		values.push_back(value);
-
-		if(isEoI())
+//Returns true if enemy is read successfully, false if it fails for any reason
+bool readEnemy(vector<string> &keys, vector<string> &values, string &line)
+{
+	do
+	{
+		string key;
+		string value;
+		if (isBlankLine(line))
 			return true;
 
-		getline(cin, line);
+		if (!getKey(key, line))
+		{
+			cerr << prgName << "- A Key is missing on line:  " << line << "\n";
+			return false;
+		}
+		if (!isAlphabetic(key))
+		{
+			cerr << prgName << "- Key:" << key << " not alphabetic!" << "\n";
+			return false;
+		}
+		if (!getValue(value, line))
+		{
+			cerr << prgName << "- Value is missing for Key: " << key << "\n";
+			return false;
+		}
+		if (!isKeyUnique(keys, key))
+		{
+			cerr << prgName << "- Key: " << key << " is not unique!" << "\n";
+			return false;
+		}
 
-	} while(!isBlankLine(line));
+		keys.push_back(key);
+		values.push_back(value);
+	} while (getline(cin, line));
 	return true;
 }
 
-int getMaxCharLength(vector<string> &keys)
-{	
-	auto length = keys[0].length();
-	for(auto &k : keys){
-	if(k.length() > length)
-		length = k.length();
+int getMaxCharLength(const vector<string> &keys)
+{
+	if (keys.empty())
+		return 0;
+
+	size_t length = 0;
+	for (const auto &k : keys)
+	{
+		if (k.length() > length)
+			length = k.length();
 	}
-	return length;
+	return static_cast<int>(length);
 }
-
-
