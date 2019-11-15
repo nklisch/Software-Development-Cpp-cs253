9a4c1820789463dd0f45030afa7ab519d300a30c
Nathan Klisch <nklisch@localhost.localdomain>
2019-11-14 21:21:37 -0700

    update

--- cs253/HW6/Gallery.cc
+++ cs253/HW6/Gallery.cc
@@ -7,29 +7,32 @@
 
 using namespace std;
 
-
-Gallery::Gallery(const string& f1, const string& f2)
+Gallery::Gallery(const string &f1, const string &f2)
 {
     files.push_back(f1);
     files.push_back(f2);
     create();
 }
-Gallery::Gallery(const string& f1, const string& f2, const string& f3)
+Gallery::Gallery(const string &f1, const string &f2, const string &f3)
 {
+    files.reserve(3);
     files.push_back(f1);
     files.push_back(f2);
     files.push_back(f3);
     create();
 }
-Gallery::Gallery(const string& f1, const string& f2, const string& f3, const string& f4)
+Gallery::Gallery(const string &f1, const string &f2, const string &f3, const string &f4)
 {
-files.push_back(f1);
+    files.reserve(4);
+    files.push_back(f1);
     files.push_back(f2);
     files.push_back(f3);
     files.push_back(f4);
     create();
 }
-Gallery::Gallery(const string& f1, const string& f2, const string& f3, const string& f4, const string& f5){
+Gallery::Gallery(const string &f1, const string &f2, const string &f3, const string &f4, const string &f5)
+{
+    files.reserve(5);
     files.push_back(f1);
     files.push_back(f2);
     files.push_back(f3);
@@ -37,8 +40,9 @@ Gallery::Gallery(const string& f1, const string& f2, const string& f3, const str
     files.push_back(f5);
     create();
 }
-Gallery::Gallery(const string& f1, const string& f2, const string& f3, const string& f4, const string& f5, const string& f6)
+Gallery::Gallery(const string &f1, const string &f2, const string &f3, const string &f4, const string &f5, const string &f6)
 {
+    files.reserve(6);
     files.push_back(f1);
     files.push_back(f2);
     files.push_back(f3);
@@ -48,83 +52,85 @@ Gallery::Gallery(const string& f1, const string& f2, const string& f3, const str
     create();
 }
 
-void Gallery::read(const string &filename, const string &keyfile)
+void Gallery::read(ifstream &inFile, const string &keyfile)
 {
-Enemy e(keyfile, this);
-ifstream inFile(filename);
-while(e.read(inFile)){
-    add(e);
-}
+    Enemy e(keyfile, this);
+    while (e.read(inFile))
+    {
+        add(e);
+    }
 }
 
-size_t Gallery::findKeyfile() const
+void Gallery::create()
 {
-    size_t key = string::npos;
-    for(size_t i = 0; i < files.size(); i++)
+    ifstream openFiles[6];
+    size_t keyfileIndex = string::npos;
+    for (size_t i = 0; i < files.size(); i++)
     {
-        if(isKeyfile(files[i]))
+        openFiles[i].open(files[i]);
+        if (openFiles[i])
+            throw runtime_error("File " + files[i] + " provided did not open");
+        if (isKeyfile(openFiles[i]))
         {
-            if(key != string::npos)
+            if (keyfileIndex != string::npos)
                 throw runtime_error("Too many keyfiles! Keyfile: " + files[i]);
-            key = i;
+            keyfileIndex = i;
         }
     }
-    if(key == string::npos)
+
+    if (keyfileIndex == string::npos)
         throw runtime_error("No keyfile provided!");
-    return key;
-}
 
-void Gallery::create(){
-    string keyfile;
-    size_t index;
-    index = findKeyfile();
-    keyfile = files[index];
-    files.erase(files.begin() + index);
-    for(const auto &f : files){
-        read(f, keyfile);
-    }
-}
-
-bool Gallery::isKeyfile(const string& filename) const
-{
-ifstream infile(filename);
-if(!infile)
-    throw runtime_error("File " + filename +" provided did not open");
-string line;
-bool flag = false;
-while(getline(infile, line))
+    openFiles[keyfileIndex].close();
+    for (size_t i = 0; i < files.size(); i++)
     {
-    if(line.empty())
-        return false;
-    if(hasBlanks(line))
-        return false;
-    if(!isAlphaNum(line))
-        return false;
-    flag = true;
+        if (i != keyfileIndex)
+        {
+            openFiles[i].seekg(ios_base::beg);
+            read(openFiles[i], files[keyfileIndex]);
+        }
+    }
+}
+
+bool Gallery::isKeyfile(ifstream &infile) const
+{
+
+    string line;
+    bool flag = false;
+    while (getline(infile, line))
+    {
+        if (line.empty())
+            return false;
+        if (hasBlanks(line))
+            return false;
+        if (!isAlphaNum(line))
+            return false;
+        flag = true;
     }
 
-return flag;
+    return flag;
 }
-Enemy * Gallery::get(size_t n)
+Enemy *Gallery::get(size_t n)
 {
-    if(n >= size()) 
+    if (n >= size())
         throw range_error("Index out of bounds: " + to_string(n) + " max is " + to_string(size()));
 
     return &collection[n];
 }
 
-const Enemy* Gallery::get(size_t n) const
+const Enemy *Gallery::get(size_t n) const
 {
-    if(n >= size()) 
+    if (n >= size())
         throw range_error("Index out of bounds: " + to_string(n) + " max is " + to_string(size()));
     return &collection[n];
 }
 
-ostream& operator<<(ostream& os, const Gallery & g)
+ostream &operator<<(ostream &os, const Gallery &g)
 {
-    for(size_t i = 0; i < g.collection.size(); i++){
+    for (size_t i = 0; i < g.collection.size(); i++)
+    {
         os << g.collection[i];
-        if(i < g.collection.size() - 1)
+        if (i < g.collection.size() - 1)
             os << '\n';
     }
     return os;
